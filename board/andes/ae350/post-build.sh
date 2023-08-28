6c8e59ceb2880074ad316e499241078146ab8a7e
Yu Chien Peter Lin <peterlin@andestech.com>
2023-08-28 10:42:03 +0800

    ae350: Add support for AST530 OpenSBI, U-Boot, and Linux
    This patch adds support for building with AST530 packages. The packages
    are sourced from Andes Technology GitHub repositories.
    
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- board/andes/ae350/post-build.sh
+++ board/andes/ae350/post-build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-cp $BINARIES_DIR/Image $TARGET_DIR/boot
-cp $BINARIES_DIR/ae350_ax45mp.dtb $TARGET_DIR/boot
+# Kernel Image and DTB are fetched via extlinux.conf
+IMAGE="${BINARIES_DIR}/Image.gz"
+DTB="${BINARIES_DIR}/andes/ax45mp_c4_d_dsp_ae350.dtb"
+
+# Check if source files exist
+if [ ! -f ${IMAGE} ] || [ ! -f ${DTB} ]; then
+    echo "Error: files do not exist"
+    exit 1
+fi
+
+# Copy files to target directory
+cp ${IMAGE} "${TARGET_DIR}/boot"
+cp ${DTB}   "${TARGET_DIR}/boot"
